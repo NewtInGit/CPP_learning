9d34e7db6f0eae0f13bb5f333bc1f9365e46c3ae
NewtInGit <50037832+NewtInGit@users.noreply.github.com>
2020-01-07 14:50:29 +0800

    Template
    重载
    显式具体化

--- Learning/template/template.cpp
+++ Learning/template/template.cpp
@@ -6,6 +6,23 @@ using namespace std;
 template <typename AnyType>
 void Swap(AnyType &a, AnyType &b);//加入&则可以改变值
 
+template <typename T>
+void Swap(T* a, T* b, int n);
+const int lim = 8;
+void Show(int a[lim]);
+
+
+struct job
+{
+	char name[40];
+	double salary;
+	int floor;
+};
+
+void ShowJob(job j);
+template<>void Swap<job>(job& j1, job& j2);
+
+
 
 int main()
 {
@@ -21,6 +38,24 @@ int main()
 	cout << "a , b = " << a << " , " << b << endl;
 	Swap(a, b);
 	cout << "Now a , b = " << a << " , " << b << endl;
+
+
+	int d1[lim] = { 0,1,2,3,4,5,6,7 };
+	int d2[lim] = { 7,6,5,4,3,2,1,0 };
+	Swap(d1, d2,lim);
+	Show(d1);
+	cout << "and" << endl;
+	Show(d2);	
+
+	job Apple = { "Software Eng",30000,5 };
+	job ChuangXiang = { "Hardware Eng",6000,2 };
+	Swap(Apple, ChuangXiang);
+	ShowJob(Apple);
+	ShowJob(ChuangXiang);
+
+
+
+
 	return 0;
 }
 
@@ -33,13 +68,49 @@ void Swap(AnyType &a, AnyType &b)
 	b = temp;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+template<typename T>
+void Swap(T a[], T b[], int n)
+{
+	T temp;
+	for (int i = 0; i < n; i++)
+	{
+		temp = a[i];
+		a[i] = b[i];
+		b[i] = temp;
+	}
+}
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+
+template<>void Swap<job>(job& j1, job& j2)//Swap函数优先使用该重载
+{
+	double t1;
+	int t2;
+	t1 = j1.salary;
+	j1.salary = j2.salary;
+	j2.salary = t1;
+	t2 = j1.floor;
+	j1.floor = j2.floor;
+	j2.floor = t2;
+}
+
+void Show(int a[lim])
+{
+	for (int i = 0; i < lim; i++)
+	{
+		std::cout << a[i] << std::endl;
+	}
+
+
+}
+
+void ShowJob(job j)
+{
+	cout << "Job Name: " << j.name << endl;
+	cout << "Job Salary: " << j.salary << endl;
+	cout << "Job Floor: " << j.floor << endl;
+
+}
+
+//模板
+//模板的重载
+//模板的显示具体化，优先选择job
